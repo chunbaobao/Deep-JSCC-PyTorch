e142bb899a78eb6343a4de5d79cdd5f17e954583
ZHENG Chunhang <84070453+chunbaobao@users.noreply.github.com>
2024-01-16 22:55:35 +0800

    Update channel.py

--- channel.py
+++ channel.py
@@ -1,15 +1,15 @@
 import torch
 import torch.nn as nn
-import numpy as np
-
 
 def channel(channel_type='AWGN', snr=20):
     def AWGN_channel(z_hat: torch.Tensor):
         if z_hat.dim() == 4:
-            k = np.prod(z_hat.size()[1:])
+            # k = np.prod(z_hat.size()[1:])
+            k = torch.prod(torch.tensor(z_hat.size()[1:]))
             sig_pwr = torch.sum(torch.abs(z_hat).square(), dim=(1, 2, 3), keepdim=True)/k
         elif z_hat.dim() == 3:
-            k = np.prod(z_hat.size())
+            # k = np.prod(z_hat.size())
+            k = torch.prod(torch.tensor(z_hat.size()))
             sig_pwr = torch.sum(torch.abs(z_hat).square())/k
         noi_pwr = sig_pwr / ( 10 ** (snr / 10))
         noise = torch.randn_like(z_hat) * torch.sqrt(noi_pwr)
